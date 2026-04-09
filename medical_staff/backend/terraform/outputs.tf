1a4f0c439e91f72d7f30257f0addb08a5622c1b0
KuyPoly <kuypoly297@gmail.com>
2026-04-09 18:17:35 +0700

    add output

--- medical_staff/backend/terraform/outputs.tf
+++ medical_staff/backend/terraform/outputs.tf
@@ -2,3 +2,8 @@ output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
   value       = aws_instance.app_server.private_dns
 }
+
+# Load Balancer output (to access your app)
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
